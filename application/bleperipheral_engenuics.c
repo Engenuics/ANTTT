ec11ad5af6f376860b2f57c70c596114849cc7fb
vipinbb <captainmohyal@gmail.com>
2017-07-22 17:13:37 -0400

    Added skeleton of bleperipheral_engenuics module

---
+++ application/bleperipheral_engenuics.c
@@ -0,0 +1,272 @@
+/**********************************************************************************************************************
+File: bleperipheral_engenuics.c                                                                
+
+Description:
+BLE Peripheral Service module for the Engenuics BLE Profile.
+**********************************************************************************************************************/
+
+#include "configuration.h"
+
+/***********************************************************************************************************************
+Global variable definitions with scope across entire project.
+All Global variable names shall start with "G_"
+***********************************************************************************************************************/
+/* New variables */
+volatile u32 G_u32BPEngenuicsFlags;                       /* Global state flags */
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Existing variables (defined in other files -- should all contain the "extern" keyword) */
+extern volatile u32 G_u32SystemFlags;                  /* From main.c */
+
+extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
+extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
+
+
+/***********************************************************************************************************************
+Global variable definitions with scope limited to this local application.
+Variable names shall start with "BPEngenuics_" and be declared as static.
+***********************************************************************************************************************/
+static u32 BPEngenuics_u32Timeout;           /* Timeout counter used across states */
+static u8  uuid_type;                        /* UUID Type for BLEEngenuics service */ 
+static u16 service_handle;                   /* Handle for the GATTS Service */
+static ble_gatts_char_handles_t tx_handles;  /* TX Characteristic Handles */
+static ble_gatts_char_handles_t rx_handles;  /* RX Characteristic Handles */          
+static u16 conn_handle;                      /* Connection handle indicating if the device is GAP Connected to Client*/
+static bool is_notification_enabled;         /* Flag to indicate if Notifications have been enabled by the Client */
+
+/**********************************************************************************************************************
+Function Definitions
+**********************************************************************************************************************/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Public functions                                                                                                   */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Protected functions                                                                                                */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsInitialize
+
+Description:
+Initializes the State Machine and its variables.
+
+Requires:
+   - Called after the stack has been initialized.
+   - Called from the BLEIntegrationHandlerInitialize
+
+Promises:
+  - Characteristics are registered to the Services.
+  - Service is registered with the stack for operations.
+*/
+bool BPEngenuicsInitialize(void)
+{
+  u32 error;
+
+  // Initialize.
+  conn_handle = BLE_CONN_HANDLE_INVALID;
+  is_notification_enabled = false;
+
+  // Add the services and characteristics.
+  error |= BPEngenuicsAddService();
+  error |= BPEngenuicsAddRxCharacteristic();
+  error |= BPEngenuicsAddTxCharacteristic();
+
+  return (erro == NRF_SUCCESS);
+} /* end BPEngenuicsInitialize() */
+
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsOnConnect
+
+Description:
+Initializes the State Machine and its variables.
+
+Requires:
+   - BPEngenuicsInitialize has been called.
+
+Promises:
+  - Notifies the module that it is in the connected state.
+*/
+void BPEngenuicsOnConnect(void)
+{
+   
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsOnDisconnect
+
+Description:
+Initializes the State Machine and its variables.
+
+Requires:
+   - BPEngenuicsInitialize has been called
+
+Promises:
+  - Notifies the module that it is in the disconnected state.
+*/
+void BPEngenuicsOnDisconnect(void)
+{
+   
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsOnWrite
+
+Description:
+Handles the Service enabling/disabling. Handles the Value Char writes from Client.
+
+Requires:
+   - Called after the module has been initialized.
+   - evt is the ble_evt_t* containing the appropriate GATTS Write information
+   
+Promises:
+  - Handles Enabling/Disabling of the BPEngenucis Service.
+  - Handles Rx message decoding of the messages received from the Client.
+*/
+void BPEngenuicsOnWrite(ble_evt_t* evt)
+{
+   
+}
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Private functions                                                                                                  */
+/*--------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsAddService
+
+Description:
+Registers the service to the
+
+Requires:
+   - Called after the module has been intialized.
+   
+Promises:
+  - Adds the BPEngenuics Rx Service.
+  - Registers a properietary 128bit Service UUID base.
+  - Followed by 16bit Service identifier. 
+*/
+static u32 BPEngenuicsAddService(void)
+{
+   u32 err;
+   ble_uuid_t ble_uuid;
+   ble_uuid128_t ble_base_uuid = BPENGENUICS_BASE_UUID;
+
+   // Add Service UUID with base .
+   err = sd_ble_uuid_vs_add(&ble_base_uuid, &uuid_type);
+
+   // Add the 16bit unique value to base.
+   ble_uuid.type = uuid_type;
+   ble_uuid.uuid = BPENGENUICS_SERVICE_UUID;
+   err = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &ble_uuid, &service_handle);
+
+   return err;
+}
+
+
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsAddRxcharacteristic
+
+Description:
+Adds the Rx Characteristic to the BLE Service.
+
+Requires:
+   - Called during module initialization
+   
+Promises:
+  - Adds the BPEngenuics Rx Characteristic.
+*/
+static u32 BPEngenuicsAddRxCharacteristic(void)
+{
+    ble_gatts_char_md_t rxchar_metadata;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+
+   // Metadata for the Rx Characteristic.
+    memset(&rxchar_metadata, 0, sizeof(rxchar_metadata));
+    rxchar_metadata.char_props.write = 1;
+    rxchar_metadata.char_props.write_wo_resp = 1;
+  
+    // Load the RX CHAR UUID.
+    ble_uuid.type = uuid_type;
+    ble_uuid.uuid = BPENGENUICS_RX_CHAR_UUID;
+
+    // Metadata for the Rx Attribute.
+    memset(&attr_md, 0, sizeof(attr_md));
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+    attr_md.vloc    = BLE_GATTS_VLOC_STACK;
+    attr_md.vlen    = 1;
+
+    // Setup of the Rx Attribute.
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+    attr_char_value.p_uuid    = &ble_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len  = BPENGENUICS_MAX_CHAR_LEN;
+    attr_char_value.max_len   = BPENGENUICS_MAX_CHAR_LEN;
+
+    return sd_ble_gatts_characteristic_add(&service_handle, &rxchar_metadata, &attr_char_value, &rx_handles);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+Function: BPEngenuicsAddTxcharacteristic
+
+Description:
+Adds the Tx Characteristic to the BLE Service.
+
+Requires:
+   - Called during module initialization.
+   
+Promises:
+  - Adds the BPEngenuics Tx Characteristic.
+*/
+static void BPEngenuicsAddTxCharacteristic(void)
+{
+    ble_gatts_char_md_t txchar_metadata;
+    ble_gatts_attr_md_t cccd_md;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+
+    // ClientConfigurationDescriptor Metadata.
+    memset(&cccd_md, 0, sizeof(cccd_md));
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+
+    // Location of CCCD characteristic is on user stack.
+    cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+
+    // Metadata for the Tx Characteristic.
+    memset(&txchar_metadata, 0, sizeof(txchar_metadata));
+    txchar_metadata.char_props.notify = 1;
+    txchar_metadata.p_cccd_md         = &cccd_md;
+
+    // Load the TX CHAR UUID.
+    ble_uuid.type = uuid_type;
+    ble_uuid.uuid = BPENGENUICS_TX_CHAR_UUID;
+
+    // Metadata for the Tx Attribute.
+    memset(&attr_md, 0, sizeof(attr_md));
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+    attr_md.vloc    = BLE_GATTS_VLOC_STACK;
+    attr_md.vlen    = 1;
+
+    // Setup of the Tx Attribute.
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+    attr_char_value.p_uuid    = &ble_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len  = BPENGENUICS_MAX_CHAR_LEN;
+    attr_char_value.max_len   = BPENGENUICS_MAX_CHAR_LEN;
+
+    return sd_ble_gatts_characteristic_add(&service_handle, &txchar_metadata, &attr_char_value, &tx_handles);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* End of File                                                                                                        */
+/*--------------------------------------------------------------------------------------------------------------------*/
